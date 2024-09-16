c199342746b76714fa311ad563b8ef4e2ae28b4c
Lazar <lazar.trifunovic@boopro.tech>
2024-09-16 22:12:44 +0200

    ...

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        next();
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        cors: {
+            origin: 'https://workshop-4i8ymvm1s.vercel.app',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+            credentials: false,
+        },
     });
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'https://workshop-4i8ymvm1s.vercel.app', // Specific origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+      credentials: false, // Enable cookies
+    },
   });
+
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With, Content-Type, Accept',
+  //   );
+  //   next();
+  // });
 
   await app.listen(3000);
 }
